a82f72fdfea324f5ded00575ce43401634566331
AndriiKozlovskyi <andrii@advasoftware.com>
2023-10-29 13:29:14 +0100

    first commit

---
+++ src/test/java/com/example/BackendParser/BackendParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BackendParser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
