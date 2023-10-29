323a8b5c94f5f29c738518b2a02397613f8f6fc4
AndriiKozlovskyi <andrii@advasoftware.com>
2023-10-29 14:11:25 +0100

    refactored, exchanges add, okx refactored

---
+++ src/main/java/com/example/backend_parser/controller/BitrueEndpoint.java
@@ -0,0 +1,32 @@
+package com.example.backend_parser.controller;
+
+import com.example.backend_parser.mapper.BitrueMapper;
+import com.example.backend_parser.mapper.Mapper;
+import com.example.backend_parser.mapper.MexcMapper;
+import com.example.backend_parser.service.ServiceEntity;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/bitrue")
+public class BitrueEndpoint {
+
+    static final String ORDER_BOOK_URL = "https://openapi.bitrue.com/api/v1/depth?symbol=";
+    static final String TRADING_PAIRS_URL = "https://openapi.bitrue.com/api/v1/exchangeInfo";
+    static final Mapper MAPPER = new BitrueMapper();
+    ServiceEntity bitrueService = new ServiceEntity(ORDER_BOOK_URL, TRADING_PAIRS_URL, MAPPER);
+
+    @GetMapping("/trading_pairs")
+    public HttpEntity<String> getTradingPairs(){
+        return bitrueService.parseTradingPairs();
+    }
+
+    @PostMapping("/order_books")
+    public HttpEntity<?> getOrderBooks(@RequestBody List<String> symbols) {
+
+        return bitrueService.parseOrderBooks(symbols, 1000);
+    }
+
+}
