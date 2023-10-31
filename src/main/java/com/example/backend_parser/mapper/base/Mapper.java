84bb4135c5c4c9522fdcc910f52db4519b2911f1
AndriiKozlovskyi <andrii@advasoftware.com>
2023-10-31 09:52:47 +0100

    mapper refactored

---
+++ src/main/java/com/example/backend_parser/mapper/base/Mapper.java
@@ -0,0 +1,52 @@
+package com.example.backend_parser.mapper.base;
+
+import com.example.backend_parser.calculations.PriceCalculator;
+import com.example.backend_parser.mapper.base.KeysMapper;
+import com.example.backend_parser.models.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.example.backend_parser.utils.JsonUtils.getJSONArray;
+import static com.example.backend_parser.utils.JsonUtils.getJSONObject;
+import static com.example.backend_parser.utils.JsonUtils.getValue;
+
+
+public class Mapper extends Converter implements IMapper {
+    protected List<String> resolvedQuoteAssets = Arrays.asList("USDT");
+    
+    public Token convertResponseToToken(String response, String symbol, int minAmount) {
+        BidsAsks orderBook = convertOrderBook(response);
+        BidAsk bidAsk = PriceCalculator.calculatePriceForLiquidity(minAmount, orderBook);
+
+        return new Token(symbol, bidAsk.getBid(), bidAsk.getAsk());
+    }
+
+    public List<BaseQuote> convertBaseQuote(String response) {
+        JSONArray symbols = getSymbols(response);
+
+        List<BaseQuote> baseQuoteList = new ArrayList<>();
+
+        for(int i = 0; i < symbols.length(); i ++) {
+            JSONObject symbolObject = getJSONObject(symbols.get(i));
+
+            String symbol = getValue(symbolObject, getKeysMapper().getSymbolKey());
+            String baseAsset = getValue(symbolObject, getKeysMapper().getBaseKey());
+            String quoteAsset = getValue(symbolObject, getKeysMapper().getQuoteKey());
+
+            if(quoteAssetAndCustomCheck(symbolObject)) {
+                baseQuoteList.add(new BaseQuote(symbol, baseAsset, quoteAsset));
+            }
+        }
+
+        return baseQuoteList;
+    }
+
+    protected boolean quoteAssetAndCustomCheck(JSONObject symbolObject) {
+        String quoteAsset = getValue(symbolObject, getKeysMapper().getQuoteKey());
+        return resolvedQuoteAssets.contains(quoteAsset.toUpperCase());
+    }
+}
