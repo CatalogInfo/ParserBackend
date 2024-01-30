7bcb1af144ba8a0459c04e91879474c5cb6f7282
AndriiKozlovskyi <andrii@advasoftware.com>
2024-01-30 23:48:36 +0100

    fixed className

--- src/main/java/com/example/backend_parser/logs/LogFactory.java
+++ src/main/java/com/example/backend_parser/logs/LogFactory.java
@@ -3,13 +3,10 @@ package com.example.backend_parser.logs;
 public class LogFactory {
     public static void makeALog(String message) {
         String time = TimeUtils.getTime();
-        System.out.println("-------NormalLog::" + time + "  ::  " + message + " -> " + getClassName());
-    }
-
-    private static String getClassName() {
         StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-
         StackTraceElement caller = stackTrace[2];
-        return caller.getFileName();
+        System.out.println("-------NormalLog::" + time + "  ::  " + message + " -> " + caller.getFileName());
     }
+
+
 }
